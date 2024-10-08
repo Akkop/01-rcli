7c41017f93a32313c6350ed553fa36bbe77a2fcc
guohaojie <1172489638@qq.com>
2024-10-09 02:04:45 +0800

    feat: Split Logic File

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,11 @@
-use std::{fs, path::Path};
-
+use anyhow::Ok;
 use clap::Parser;
-use csv::Reader;
-use serde::{Deserialize, Serialize};
+use rcli::{process_csv, Opts, SubCommand};
 
-#[derive(Debug, Parser)]
-#[command(name="rcli",version,author,about,long_about = None)]
-struct Opts {
-    #[command(subcommand)]
-    cmd: SubCommand,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-
-struct Iris {
-    #[serde(rename = "Sepal.Length")]
-    sepal_length: String,
-    #[serde(rename = "Sepal.Width")]
-    sepal_width: String,
-    #[serde(rename = "Petal.Length")]
-    setal_length: String,
-    #[serde(rename = "Petal.Width")]
-    setal_width: String,
-    #[serde(rename = "Species")]
-    species: String,
-}
-
-#[derive(Debug, Parser)]
-struct CsvOpts {
-    #[arg(short, long, value_parser = verify_input_file)]
-    input: String,
-
-    #[arg(short, long, default_value = "output.json")]
-    output: String,
-
-    #[arg(short, long, default_value_t = ',')]
-    delimiter: char,
-
-    #[arg(long, default_value_t = true)]
-    header: bool,
-}
-
-#[derive(Debug, Parser)]
-enum SubCommand {
-    #[command(name = "csv", about = "Show CSV,or Convert CSV to JSON")]
-    Csv(CsvOpts),
-}
-
-fn verify_input_file(filename: &str) -> Result<String, String> {
-    if Path::new(filename).exists() {
-        Ok(filename.into())
-    } else {
-        Err("File does not exist".into())
-    }
-}
-
-fn main() -> anyhow::Result<()> {
+fn main()->anyhow::Result<()>{
     let opts: Opts = Opts::parse();
     match opts.cmd {
-        SubCommand::Csv(opts) => {
-            let mut reader = Reader::from_path(opts.input)?;
-            let mut ret = Vec::with_capacity(300);
-            for result in reader.deserialize::<Iris>() {
-                let record: Iris = result?;
-                ret.push(record);
-            }
-
-            let json = serde_json::to_string_pretty(&ret)?;
-            fs::write(opts.output, json)?;
-        }
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?
     }
     Ok(())
 }
---
+++ src/process.rs
@@ -0,0 +1,32 @@
+use csv::Reader;
+use serde::{Deserialize, Serialize};
+use anyhow::Result;
+use std::fs;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Iris {
+    #[serde(rename = "Sepal.Length")]
+    sepal_length: String,
+    #[serde(rename = "Sepal.Width")]
+    sepal_width: String,
+    #[serde(rename = "Petal.Length")]
+    setal_length: String,
+    #[serde(rename = "Petal.Width")]
+    setal_width: String,
+    #[serde(rename = "Species")]
+    species: String,
+}
+
+
+pub fn process_csv(input:&str,output:&str) -> Result<()>{
+    let mut reader = Reader::from_path(input)?;
+    let mut ret= Vec::with_capacity(300);
+    for result in reader.deserialize::<Iris>() {
+        let record: Iris = result?;
+        ret.push(record);
+    }
+
+    let json = serde_json::to_string_pretty(&ret)?;
+    fs::write(output, json)?;
+    Ok(())
+}
